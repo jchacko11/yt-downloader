a403536b6fae503474873c3c0ddbae914d1930a7
jchacko11 <jonathan.j.chacko@gmail.com>
2020-04-28 17:03:45 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+
+app = Flask('app')
+from pytube import YouTube
+from threading import Thread
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/watch')
+def hello_world():
+    url = False
+    try:
+        v = request.args["v"]
+    except:
+        try:
+            url = request.args["url"]
+        except:
+            v = "4OnwUz-I2GI"
+
+    try:
+        if url:
+            yt = YouTube(url)
+        else:
+            yt = YouTube("?v=" + v)
+    except:
+        return render_template("vid_not_found.html")
+
+    streams = yt.streams
+    title = yt.title
+    thumbnail = yt.thumbnail_url
+    author = yt.author
+    views = yt.views
+
+    only_audio = streams.filter(only_audio=True)
+    only_video = streams.filter(only_video=True)
+    progressive = streams.filter(progressive=True)
+
+    primary = streams.get_highest_resolution()
+
+    return render_template(
+        "video.html",
+        streams=streams,
+        title=title,
+        thumbnail=thumbnail,
+        primary=primary,
+        author=author,
+        views=views,
+        only_audio=only_audio,
+        only_video=only_video,
+        progressive=progressive
+    )
+
+
+def run():
+    app.run()
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+# keep_alive()
+run()
---
+++ static/script.js
@@ -0,0 +1,27 @@
+$('td:contains("None")').each(function () {
+    $(this).html($(this).html().split("None").join(""));
+});
+
+$(document).ready(function () {
+
+    // Check for click events on the navbar burger icon
+    $(".navbar-burger").click(function () {
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
+
+    });
+
+    var length = $(".field.has-addons").length
+    if (length > 1) {
+        $(".field.has-addons:first").remove()
+    }
+    $("#enter").click(function () {
+        $("#link").parent().addClass("is-loading")
+        window.location = "/watch?url=" + $("#link").val()
+    })
+
+
+});
+
