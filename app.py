247be464eed09ed2e44fbe26f9bbc578ded8305a
jchacko11 <jonathan.j.chacko@gmail.com>
2020-04-29 02:58:22 -0500

    Debugging

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def hello_world():
         else:
             yt = YouTube("?v=" + v)
     except:
-        return render_template("vid_not_found.html", yt=yt, v=v)
+        return render_template("vid_not_found.html")
 
     streams = yt.streams
     title = yt.title
